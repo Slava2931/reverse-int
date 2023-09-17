e06bf08bec8ea3208fdf7a1b954b7c8ff7bd2919
Slava Anop <135474267+Slava2931@users.noreply.github.com>
2023-09-17 15:43:46 -0600

    feat : solution of the problem

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (n){
+    let Number = Array.from(String(n));
+    let reversedArray = Number.reduce((acc, Number) =>
+        [Number, ...acc], []);
+    return parseInt(reversedArray.join(''));
+
 }
